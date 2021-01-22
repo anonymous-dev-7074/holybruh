d3c50faeaf1f4ba49cef17770ac0b4d21e38ebef
kkjust kk <73049664+anonymous-dev-7074@users.noreply.github.com>
2021-01-22 16:51:14 +0530

    Update faith.js

--- faith.js
+++ faith.js
@@ -662,4 +662,4 @@ client.on("guildMemberAdd", async (member) => {
   } //ab setup/welcome.js main ayo
 })
 */
-client.login("NzY4NTE3MjI2NjU0ODU5Mjk1.X5BncQ.RMW0Gm8ioj5c0MGukIR9T67NyrA");
+client.login("");
